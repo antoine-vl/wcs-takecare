19b90a5a808666272cfad6ee6ba7876276dd2172
Benoît Moëns <benoit.moens@hotmail.com>
2019-12-17 14:41:34 +0100

    Hero Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import HeroHeader from './components/HeroHeader/HeroHeader';
+import GroupedButtons from './components/HeroHeader/SimpleSelect';
+import SimpleSelect from './components/HeroHeader/SimpleSelect';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <HeroHeader /> 
     </div>
   );
 }
---
+++ src/components/HeroHeader/SimpleSelect.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles(theme => ({
+  button: {
+    display: 'block',
+    marginTop: theme.spacing(2),
+  },
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  },
+}));
+
+export default function ControlledOpenSelect() {
+  const classes = useStyles();
+  const [langue, setlangue] = React.useState('FR');
+  const [open, setOpen] = React.useState(false);
+
+  const handleChange = event => {
+    setlangue(event.target.value);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <Select
+          labelId="demo-controlled-open-select-label"
+          id="demo-controlled-open-select"
+          open={open}
+          onClose={handleClose}
+          onOpen={handleOpen}
+          value={langue}
+          onChange={handleChange}
+          >
+          <MenuItem value={'FR'}>FR</MenuItem>
+          <MenuItem value={'EN'}>EN</MenuItem>
+          <MenuItem value={'NL'}>NL</MenuItem>
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
+    
+      
+
