c7ffdf6c4cd8265b82651d14bc39737940e11ecf
Cheldumi <cheldumi3@gmail.com>
2019-12-19 17:04:44 +0100

    merge chaly benoit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import HeroHeader from './components/HeroHeader/HeroHeader';
 import GroupedButtons from './components/HeroHeader/SimpleSelect';
 import SimpleSelect from './components/HeroHeader/SimpleSelect';
+
 function App() {
   return (
     <div className="App">
